3443e30f6469ff98191fe7552093e7bc0eac58c2
Rolo <rolandofender@gmail.com>
2024-02-14 07:12:00 -0600

    Adjusting Matches Component

--- src/app/matches-page/matches-page.component.ts
+++ src/app/matches-page/matches-page.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { CreateUserComponent } from '../homepage/create-user/create-user.component';
-import { User } from '../homepage/create-user/user.model';
-import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-matches-page',
